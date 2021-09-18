a384ae4986fd898736e7688a2796284d54f37943
André Costa <arc.andrecosta@outlook.com>
2021-09-17 21:30:54 -0300

    Adição da regra de negócio.

--- app/src/main/java/com/github/andrercosta/gastosviagem/MainActivity.kt
+++ app/src/main/java/com/github/andrercosta/gastosviagem/MainActivity.kt
@@ -2,10 +2,53 @@ package com.github.andrercosta.gastosviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
     }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if (id == R.id.buttonCalculate) {
+            calculate();
+        }
+    }
+
+    private fun calculate() {
+        if (validationOk()) {
+
+            try {
+                val distance = editDistance.text.toString().toFloat()
+                val price = editPrice.text.toString().toFloat()
+                val autonomy = editAutonomy.text.toString().toFloat()
+
+                val totalValue = (distance * price) / autonomy
+
+                textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(this, getString(R.string.informe_valores_validos), Toast.LENGTH_LONG)
+                    .show();
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.preencha_todos_campos), Toast.LENGTH_LONG)
+                .show();
+        }
+    }
+
+    private fun validationOk(): Boolean {
+        return (!editDistance.text.toString().isNullOrEmpty()
+                && !editPrice.text.toString().isNullOrEmpty()
+                && !editAutonomy.text.toString().isNullOrEmpty())
+    }
+
 }
